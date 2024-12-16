8c1ecba078cd12225c3446710a901f1401d386ce
Siasiy <sxk22@mails.tsinghua.edu.cn>
2024-12-16 23:42:59 +0800

    2337

--- generate.py
+++ generate.py
@@ -31,7 +31,22 @@ def generate(model, device, seed_words, input_length, output_length, vocab, temp
         # achieve our goal.                                                            #
         ################################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
+        seed_words_array = vocab.words_to_array(seed_words)
+        current_input = seed_words_array.to(device)
+        output_arr = seed_words_array.to(device)
+        while len(output_arr) < output_length and current_input[-1].item() != vocab.words_to_array('#')[0].item():
+            seed_probabilities = model.inference(current_input,temperature)
+            if strategy == 'sampling':
+                next_token = torch.multinomial(seed_probabilities, 1).item()
+            elif strategy == 'greedy':
+                next_token =torch.argmax(seed_probabilities).item()
+            output_arr = torch.cat((output_arr,torch.tensor([next_token]).to(device)))
+            if len(current_input) +1 > input_length:
+                current_input = current_input[1:]
+                current_input = torch.cat((current_input,torch.tensor([next_token]).to(device)))
+            else:
+                current_input = torch.cat((current_input,torch.tensor([next_token]).to(device)))
+        output_arr = output_arr.tolist()
         pass
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
@@ -74,6 +89,13 @@ if __name__=="__main__":
     temperature = 1.0
     # Experiment with different strategies.
     strategy = 'sampling'
+    #seed_words_array = vocab.words_to_array(seed_words)
+    #current_input = seed_words_array
+    #output_arr = seed_words_array
+    #next_token = torch.tensor([42])
+    #output_arr = torch.cat((output_arr,next_token))
+    #print(output_arr)
+    
 
     generated_sentence, array_length = generate(model, device, seed_words, SEQUENCE_LENGTH, 2000, vocab, temperature, strategy)
     print("Generated:\n", generated_sentence)
